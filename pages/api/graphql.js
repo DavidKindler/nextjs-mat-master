9d093b3ca56c4e37a02f8a3b70eb097d3e5c2a06
David Kindler <david.kindler@nxp.com>
2020-05-05 15:21:05 -0500

    setup models and graphql resolvers

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,26 +1,13 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
 import Cors from 'micro-cors'
-const Sequelize = require('sequelize')
-import DataLoader from 'dataloader'
 // import knex from "knex";
-const {
-  getAppsAndRoles,
-  Users,
-  Rights,
-  Roles,
-  Apps,
-  APPS,
-  ROLES
-} = require('../../lib/db')
-
-// export default (req, res) => {
-//   res.status(200).json({ text: 'Hello graphql' })
-// }
+const { Models } = require('../../lib/db')
 
 const typeDefs = gql`
   type Query {
     apps: [App]
     roles: [Role]
+    users: [User]
   }
 
   type App {
@@ -35,119 +22,46 @@ const typeDefs = gql`
     role: String!
     appId: ID!
   }
+  type User {
+    id: ID!
+    email: String!
+    username: String
+    provider: String
+  }
 `
 
 const resolvers = {
   Query: {
-    apps: async () => {
-      return await Apps.findAll()
+    apps: async (_parent, args, _context) => {
+      return await _context.Models.Apps.findAll()
     },
-    roles: async () => {
-      return await Roles.findAll()
+    roles: async (_parent, args, _context) => {
+      return await _context.Models.Roles.findAll()
+    },
+    users: async (_parent, args, _context) => {
+      return await _context.Models.Users.findAll()
     }
   }
-  // apps: async (_parent, args, _context) => {
-  //   return await Apps.findAll()
-  // }
-  // }
 }
 
-// const typeDefs = gql`
-//   type Query {
-//     albums(first: Int = 25, skip: Int = 0): [Album!]!
-//   }
-//   type Artist {
-//     id: ID!
-//     name: String!
-//     url: String!
-//     albums(first: Int = 25, skip: Int = 0): [Album!]!
-//   }
-//   type Album {
-//     id: ID!
-//     name: String!
-//     year: String!
-//     artist: Artist!
-//   }
-// `
-
-// const resolvers = {
-//   Query: {
-//     albums: (_parent, args, _context) => {
-//       return db
-//         .select('*')
-//         .from('albums')
-//         .orderBy('year', 'asc')
-//         .limit(Math.min(args.first, 50))
-//         .offset(args.skip)
-//     }
-//   },
-
-//   Album: {
-//     id: (album, _args, _context) => album.id,
-//     artist: (album, _args, { loader }) => {
-//       // return db
-//       //   .select("*")
-//       //   .from("artists")
-//       //   .where({ id: album.artist_id })
-//       //   .first();
-//       return loader.artist.load(album.artist_id)
-//     }
-//   },
-
-//   Artist: {
-//     id: (artist, _args, _context) => artist.id,
-//     albums: (artist, args, _context) => {
-//       return db
-//         .select('*')
-//         .from('albums')
-//         .where({ artist_id: artist.id })
-//         .orderBy('year', 'asc')
-//         .limit(Math.min(args.first, 50))
-//         .offset(args.skip)
-//     }
-//   }
-// }
-
-// const loader = {
-//   apps: new DataLoader(() => {
-//     return [
-//       {
-//         id: '123',
-//         app: 'App Name',
-//         url: 'http://goobar'
-//       },
-//       {
-//         id: '234',
-//         app: 'foobar',
-//         url: 'http://foobar'
-//       }
-//     ]
-//   })
-// }
-
-// const loader = {
-//   apps: new DataLoader(async () => {
-//     return await Apps.findAll()
-//   })
-// }
-
 const cors = Cors({
-  allowMethods: ['GET', 'POST', 'OPTIONS']
+  allowMethods: ['GET', 'POST', 'DELETE']
 })
 
 const apolloServer = new ApolloServer({
   typeDefs,
-  resolvers
-  // context: () => {
-  //   return { loader }
-  // }
+  resolvers,
+  context: () => {
+    return { Models }
+  }
 })
 
 const handler = apolloServer.createHandler({ path: '/api/graphql' })
 
 export const config = {
+  // not sure what this does but it is needed
   api: {
-    bodyParser: false
+    bodyParser: false // do not change. graphql api will not respond if set to true
   }
 }
 
