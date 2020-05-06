e81f506c89e24da4c8e43700bb94d1e38e39b2af
David Kindler <david.kindler@nxp.com>
2020-05-06 16:14:22 -0500

    add mutations

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -5,18 +5,51 @@ const { Models, Op } = require('../../lib/db')
 
 const typeDefs = gql`
   type Query {
-    apps(input: AppInput): [App]
+    apps: [App]
+    app(input: AppInput!): App
     roles: [Role]
     users: [User]
+    user(input: UserInput!): User
+  }
+  type Mutation {
+    newApp(input: NewAppInput!): [App]!
+    editAppUrl(input: EditAppUrlInput!): [App]!
   }
 
   type App {
     id: ID!
     app: String!
     url: String
+    roles: [Role]
   }
+
+  input NewAppInput {
+    app: String!
+    url: String!
+  }
+
+  input NewRoleInput {
+    app: String!
+    role: String!
+  }
+
+  input EditAppUrlInput {
+    id: ID!
+    url: String!
+  }
+
+  input EditRoleInput {
+    app: String
+    role: String
+  }
+
   input AppInput {
-    name: String!
+    id: ID
+    app: String
+  }
+
+  input UserInput {
+    id: ID!
   }
 
   type Role {
@@ -25,33 +58,120 @@ const typeDefs = gql`
     role: String!
     appId: ID!
   }
+
   type User {
-    id: ID!git st
+    id: ID!
     email: String!
     username: String
     provider: String
+    rights: [Right]
+  }
+
+  type Right {
+    id: ID!
+    userId: ID!
+    username: String!
+    email: String
+    app: String!
+    url: String
+    role: String
   }
 `
 
 const resolvers = {
+  Mutation: {
+    newApp: async (_parent, { input }, _context) => {
+      return await _context.Models.Apps.findOrCreate({
+        where: { app: input.app },
+        include: [{ model: _context.Models.Roles }],
+        defaults: {
+          app: input.app,
+          url: input.url,
+          roles: [
+            { app: input.app, role: 'ADMIN' },
+            { app: input.app, role: 'USER' }
+          ]
+        }
+      }).spread((app, created) => {
+        if (created) {
+          return _context.Models.Apps.findAll({
+            include: [{ model: _context.Models.Roles }],
+            order: [
+              [{ model: _context.Models.Roles, as: 'Roles' }, 'role', 'ASC']
+            ]
+          })
+        } else {
+          return new Error('app already exists')
+        }
+      })
+    },
+    editAppUrl: async (_parent, { input }, _context) => {
+      return await _context.Models.Apps.update(
+        {
+          url: input.url
+        },
+        {
+          where: { id: input.id }
+        }
+      ).then(async () => {
+        return await _context.Models.Apps.findAll({
+          include: [{ model: _context.Models.Roles }],
+          order: [
+            [{ model: _context.Models.Roles, as: 'Roles' }, 'role', 'ASC']
+          ]
+        })
+      })
+    }
+  },
   Query: {
     apps: async (_parent, { input }, _context) => {
-      console.log('args', input)
       if (input && input.name) {
         return await _context.Models.Apps.findAll({
+          include: [{ model: _context.Models.Roles }],
           where: {
             app: input.name
           }
         })
       } else {
-        return await _context.Models.Apps.findAll()
+        return await _context.Models.Apps.findAll({
+          include: [{ model: _context.Models.Roles }]
+        })
       }
+    },
+
+    app: async (_parent, { input }, _context) => {
+      let id = input.id ? input.id : null
+      let app = input.app ? input.app : null
+      return await _context.Models.Apps.findOne({
+        where: {
+          [Op.or]: [{ id: id }, { app: app }]
+        }
+      })
     },
     roles: async (_parent, args, _context) => {
       return await _context.Models.Roles.findAll()
     },
-    users: async (_parent, args, _context) => {
-      return await _context.Models.Users.findAll()
+    users: async (_parent, { input }, _context) => {
+      if (input && input.id) {
+        return await _context.Models.Users.findAll({
+          include: [{ model: _context.Models.Rights }],
+          where: {
+            id: input.id
+          }
+        })
+      } else {
+        return await _context.Models.Users.findAll({
+          include: [{ model: _context.Models.Rights }]
+        })
+      }
+    },
+    user: async (_parent, { input }, _context) => {
+      return await _context.Models.Users.findOne({
+        include: [{ model: _context.Models.Rights }],
+        where: {
+          id: input.id
+        }
+      })
     }
   }
 }
